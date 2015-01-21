c8098c79c482eebe7c4114686f155145c52cc5ca
Mikkel Bergmann <mikkel@jsguy.com>
2015-01-21 17:37:47 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,97 @@
+var validator = require('validator');
+
+/* 	This binder allows you to create a validation method on a model, (plain 
+	javascript function that defines some properties), that can return a set 
+	of error messages for invalid values.
+	
+	The validations are from https://github.com/chriso/validator.js	
+
+	## Example
+
+	Say you have an object like so:
+
+		var User = function(){
+			this.name = "bob";
+			this.email = "bob_at_email.com";
+		}, user = new User();
+
+	Now if you wanted to create an isValid function that can be used to ensure 
+	you don't have an invalid email address, you simply add:
+
+
+	To your model, so you get:
+
+		var User = function(){
+			this.name = "bob";
+			this.email = "bob_at_email.com";
+			this.isValid = modelbinder.bind(this, {
+				email: {
+					'isEmail': "Must be a valid email address"
+				}
+			});
+		}, user = new User();
+
+	Then just call the `isValid` method to see if it is valid - if it is
+	invalid, (as it will be in this case), you will get an object like so:
+
+		user.isValid()
+		//	Returns: { email: ["Must be a valid email address"] }
+
+	You can also check if a particular field is valid like so:
+
+		user.isValid('email');
+
+ */
+module.exports = {
+	bind: function(self, vObj){
+		return function(name){
+			var result = {},
+				//	For some reason node-validator doesn't have this...
+				isNotEmpty = function(value){
+					return typeof value !== "undefined" && value !== "" && value !== null;
+				},
+				//	Get value of property from 'self', which can be a function.
+				getValue = function(name){
+					return typeof self[name] == "function"? self[name](): self[name];
+				},
+				//	Validates a value against a set of validations
+				//	Returns true if the value is valid, or an object 
+				validate = function(name, value, validations) {
+					var validation,
+						tmp,
+						result = [];
+					for(validation in validations) {
+						if(validation == "isRequired") {
+							//	use our "isRequired" function
+							tmp = isNotEmpty(value)? true: validations[validation]; 
+						} else {
+							//	Use validator method
+							tmp = validator[validation](value)? true: validations[validation]; 
+						}
+
+						//	Handle multiple messages
+						if(tmp !== true) {
+							result = (result === true || result == "undefined")? []: result;
+							result.push(tmp);
+						} else {
+							result = true;
+						}
+					}
+					return result;
+				};
+
+			if(name) {
+				result = validate(name, getValue(name), vObj[name]);
+			} else {
+				//	Validate the whole model
+				for(name in vObj) {
+					result[name] = validate(name, getValue(name), vObj[name]);
+				}
+			}
+
+			console.log('validate', name, result);
+
+			return result;
+		}
+	}
+};
